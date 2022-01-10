57452cd553c42c4cd4386549aeb9497cc0bcf2c7
Admin <Serger9393@mail.ru>
2022-01-10 23:20:47 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -1,9 +1,34 @@
 package ru.job4j.oop;
 
 public class Dog {
+
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String food) {
+        this.food = food;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Dog polkan = new Dog();
+        Dog polcan = new Dog();
+        polcan.giveNick("polcan");
+        polcan.eat("Котлета");
+        polcan.show();
         Dog sharik = new Dog();
+        sharik.giveNick("sharik");
+        sharik.eat("Сосиска");
+        sharik.show();
         Dog zhychka = new Dog();
+        zhychka.giveNick("zhychka");
+        zhychka.eat("Тапок");
+        zhychka.show();
     }
 }
