5fb95ba297d114b14c587c4043e0a296583e940b
Admin <Serger9393@mail.ru>
2022-01-20 22:07:37 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item time = new Item();
+        System.out.println(time.getCreated());
+    }
+}
