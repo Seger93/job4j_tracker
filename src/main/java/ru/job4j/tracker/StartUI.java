9ffffcd5f41ddccac6d985abcb02b00bdf00d733
Admin <Serger9393@mail.ru>
2022-01-23 04:33:42 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,12 @@
 package ru.job4j.tracker;
-import java.time.format.DateTimeFormatter;
+
 import java.time.LocalDateTime;
 
 public class StartUI {
     public static void main(String[] args) {
         Item time = new Item();
-        LocalDateTime currentDateTime = time.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String currentDateTimeFormat = currentDateTime.format(formatter);
-        System.out.println(currentDateTimeFormat);
+        System.out.println(time);
     }
 }
+
+
