9f909d07384a879ced87f10735822b31ad65229b
Admin <Serger9393@mail.ru>
2022-01-29 18:45:05 +0300

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -177,7 +177,7 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is("Menu."
                 + System.lineSeparator()
-                + "0. Exit" + System.lineSeparator()
+                + "0. Exit " + System.lineSeparator()
                 + "Exit tracker" + System.lineSeparator()
         ));
     }
